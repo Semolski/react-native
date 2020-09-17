bc0e42a16458a275822808810404522fae8b9658
Sarah Mayoral-Osmolski <sarah@semolski.com>
2020-09-17 11:07:42 -0500

    React Navigation
    Created a side drawer set up with React Navigation's Drawer Navigator support.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './components/MainComponent';
 
+
 export default class App extends React.Component {
   render() {
     return (
