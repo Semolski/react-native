f5aaac57475b65c1fa59031c3334b6fbd0186533
Sarah Mayoral-Osmolski <sarah@semolski.com>
2020-09-22 11:16:51 -0500

    Added Favorites Component
    Show the list of favorites for the user
    Navigate to the details of the dish when the users taps a favorite

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from "./DishdetailComponent";
 import Contact from "./ContactComponent";
+import Favorites from "./FavoriteComponent";
 import About from "./AboutComponent";
 import {View, Platform, Image, StyleSheet, ScrollView, Text} from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
@@ -122,6 +123,24 @@ const ReservationNavigator = createStackNavigator ({
     })
 })
 
+const FavoritesNavigator = createStackNavigator ({
+    Favorites: {screen: Favorites}
+}, {
+    navigationOptions: ({navigation }) => ({
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff",
+        headerLeft: <Icon name='menu' size={24}
+                          color="white"
+                          onPress={() => navigation.toggleDrawer()}
+        />
+    })
+})
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container}
@@ -201,6 +220,21 @@ const MainNavigator = createDrawerNavigator({
             )
         }
     },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                title: 'My Favorites',
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({ tintColor}) => (
+                    <Icon
+                        name='heart'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
         Reservation: {
             screen: ReservationNavigator,
             navigationOptions: {
